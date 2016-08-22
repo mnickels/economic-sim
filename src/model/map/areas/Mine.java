f1c8fb59fcd3dc0335eb2200c6e0a9274cfc28ee
mnickels <mnickels@uw.edu>
2016-08-22 12:59:19 -0700

    Added the Mine improvement as an example of an Improvement

--- src/model/map/areas/Mine.java
+++ src/model/map/areas/Mine.java
@@ -13,16 +13,17 @@ public class Mine implements Improvement {
 	@Override
 	public int[] getCost() {
 		int[] cost = new int[Resource.values().length];
-		cost[Resource.MONEY.ordinal()] = 5000;		// 5000 gold cost
-		cost[Resource.WOOD.ordinal()] = 600;		// 600 wood cost
-		cost[Resource.FOOD.ordinal()] = 500;		// 500 food cost
+		cost[Resource.MONEY.ordinal()] = 500;		// 5000 gold cost
+		cost[Resource.WOOD.ordinal()] = 60;		// 600 wood cost
+		cost[Resource.FOOD.ordinal()] = 50;		// 500 food cost
 		return cost;
 	}
 
 	@Override
 	public int[] getYield(int[] resources) {
-		// TODO
-		return null;
+		int[] yield = new int[Resource.values().length];
+		yield[Resource.WOOD.ordinal()] = (int) (resources[Resource.WOOD.ordinal()] * 0.05);		// 5% increase in wood yield
+		return yield;
 	}
 
 }
